441d77797fbc7320092ba7089e910989f1bf89fd
NicolasARS <aluriv9412@ieselcaminas.org>
2023-03-01 13:45:58 +0100

    Codigo Refactorizado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,60 +1,75 @@
 package com.company;
 import java.util.Scanner;
-public class Main    {
+
+public class Main {
     public static void main(String[] args) {
-        Scanner teclado=new Scanner(System.in);
-        System.out.println("Introduce el número para la criba de Erastótenes:");
-        int dato=teclado.nextInt();
-        int vector[]=new int[dato];
-        System.out.println("\nVector inicial hasta :"+dato);
-        for (int i = 0; i < vector.length; i++) {
-            if (i%10==0) System.out.println();
-            System.out.print(i+1+"\t");
-        }
-        vector=generarPrimos(dato);
-        System.out.println("\nVector de primos hasta:"+dato);
-        for (int i = 0; i < vector.length; i++) {
-            if (i%10==0) System.out.println();
-            System.out.print(vector[i]+"\t");
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce el número para la criba de Eratóstenes: ");
+        int numero = scanner.nextInt();
+
+        int[] primos = generarPrimos(numero);
+
+        System.out.println("\nVector inicial hasta " + numero + ":");
+        imprimirNumerosHasta(numero);
+
+        System.out.println("\nVector de primos hasta " + numero + ":");
+        imprimirNumeros(primos);
+        System.out.println();
     }
-    // Generar números primos de 1 a max
-    public static int[] generarPrimos (int max)
-    {
-        int i,j;
-        if (max >= 2) {
-            // Declaraciones
-            int dim = max + 1; // Tamaño del array
-            boolean[] esPrimo = new boolean[dim];
-            // Inicializar el array
-            for (i=0; i<dim; i++)
-                esPrimo[i] = true;
-            // Eliminar el 0 y el 1, que no son primos
-            esPrimo[0] = esPrimo[1] = false;
-            // Criba
-            for (i=2; i<Math.sqrt(dim)+1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (j=2*i; j<dim; j+=i)
-                        esPrimo[j] = false;
+
+    // Generar números primos de 1 a maximo
+    public static int[] generarPrimos(int maximo) {
+        if (maximo < 2) {
+            return new int[0]; // Vector vacío
+        }
+
+        boolean[] esPrimo = new boolean[maximo + 1];
+        for (int i = 2; i <= maximo; i++) {
+            esPrimo[i] = true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(maximo); i++) {
+            if (esPrimo[i]) {
+                for (int j = i * i; j <= maximo; j += i) {
+                    esPrimo[j] = false;
                 }
             }
-            // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (i=0; i<dim; i++) {
-                if (esPrimo[i])
-                    cuenta++;
+        }
+
+        int cantidad = 0;
+        for (int i = 2; i <= maximo; i++) {
+            if (esPrimo[i]) {
+                cantidad++;
             }
-            // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-            for (i=0, j=0; i<dim; i++) {
-                if (esPrimo[i])
-                    primos[j++] = i;
+        }
+
+        int[] primos = new int[cantidad];
+        for (int i = 2, j = 0; i <= maximo; i++) {
+            if (esPrimo[i]) {
+                primos[j++] = i;
             }
-            return primos;
-        } else { // max < 2
-            return new int[0];
-            // Vector vacío
+        }
+
+        return primos;
+    }
+
+    // Imprimir números de 1 a máximo, 10 por línea
+    public static void imprimirNumerosHasta(int maximo) {
+        for (int i = 1; i <= maximo; i++) {
+            if (i > 1 && i % 10 == 1) {
+                System.out.println();
+            }
+            System.out.print(i + "\t");
+        }
+    }
+
+    // Imprimir números, 10 por línea
+    public static void imprimirNumeros(int[] numeros) {
+        for (int i = 0; i < numeros.length; i++) {
+            if (i > 0 && i % 10 == 0) {
+                System.out.println();
+            }
+            System.out.print(numeros[i] + "\t");
         }
     }
 }
